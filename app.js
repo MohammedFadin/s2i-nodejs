175378f8eaf8f858828577f7dd3e69847291d7da
Mohammed Fadin <MohammedFadin@users.noreply.github.com>
2020-08-22 15:13:37 +0400

    initial upload

---
+++ app.js
@@ -0,0 +1,9 @@
+http = require("http");
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello! I have been deployed by Fadin -- v5");
+    res.end();
+  })
+  .listen(8080);
